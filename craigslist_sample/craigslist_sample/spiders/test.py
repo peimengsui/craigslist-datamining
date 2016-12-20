66106e350143b0cfadfddf53f6031765468fd1fd
peimengsui <peimengsui@gmail.com>
2016-12-20 13:59:10 -0500

    finish

---
+++ craigslist_sample/craigslist_sample/spiders/test.py
@@ -0,0 +1,21 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from craigslist_sample.items import CraigslistSampleItem
+
+class MySpider(BaseSpider):
+    name = "craig"
+    allowed_domains = ["newyork.craigslist.org"]
+    start_urls = ["https://newyork.craigslist.org/search/sss"]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        titles = hxs.xpath("//span[@class='pl']")
+        items = []
+        for titles in titles:
+            item = CraigslistSampleItem()
+            item["title"] = titles.select("a/text()").extract()
+            item["link"] = titles.select("a/@href").extract()
+            items.append(item)
+        return items
+
+
